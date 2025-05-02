17af01d2540809abdafeb3451cc24845b7997288
Ankit Patel <ankitpatel657843@gmail.com>
2025-05-02 17:48:20 +0530

    Update API endpoints to use local server for user login and registration

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,13 @@ import userRoute from "./routes/userRoutes.js";
 import postRoute from "./routes/postRoutes.js";
 import messageRoute from "./routes/messageRoutes.js";
 import { app, server } from "./socket/socket.js";
-import path from "path";
+
  
 dotenv.config();
 
 
 const PORT = process.env.PORT || 3000;
 
-const __dirname = path.resolve();
 
 //middlewares
 app.use(express.json());
@@ -30,12 +29,6 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
-
-
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-})
 
 
 server.listen(PORT, () => {
