fdae5be7d99324bf461a39b4f2e959115f1a9153
Ankit Patel <ankitpatel657843@gmail.com>
2025-05-02 22:47:06 +0530

    Update production server URL in axios instance configuration

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
-let IS_PROD = false;
+let IS_PROD = true;
 const server = IS_PROD
-  ? "https://the-cmdian-memories.onrender.com"
+  ? "https://the-cm-dian-memories.vercel.app"
   : "http://localhost:5000";
 
 export default server;
